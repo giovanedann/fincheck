6711da5054d2862a5b6f37878c002aa7ee021c78
Giovane <giovanedaniell@gmail.com>
2023-07-02 15:59:02 -0300

    refactor: move user creation to the auth resource

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { compare } from 'bcryptjs';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { compare, hash } from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 
 import { AuthenticateDto } from './dto/authenticate.dto';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { SignUpDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
@@ -30,5 +36,52 @@ export class AuthService {
     const accessToken = await this.jwtService.signAsync({ sub: user.id });
 
     return { accessToken };
+  }
+
+  async signUp({ email, name, password }: SignUpDto) {
+    const isEmailAlreadyTaken = await this.usersRepository.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (isEmailAlreadyTaken) {
+      throw new ConflictException('This e-mail is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepository.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+
+              // expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
+
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 }
