8d18ba95fbb5fed4a9b0b9bcad0c08064adf023b
Giovane <giovanedaniell@gmail.com>
2023-07-02 16:57:13 -0300

    feat: add /users/me endpoint to retrieve user data from access token

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -4,4 +4,17 @@ import { UsersRepository } from 'src/shared/database/repositories/users.reposito
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
+
+  async getUserById(userId: string) {
+    const user = await this.usersRepository.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
 }
