76e02c6609c9b53fa9be35649b21a3bc67b3c2e8
Giovane <giovanedaniell@gmail.com>
2023-07-02 10:30:33 -0300

    feat(services): add create method of user service

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create({ email, name, password }: CreateUserDto) {
+    const isEmailAlreadyTaken = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (isEmailAlreadyTaken) {
+      throw new ConflictException('This e-mail is already in use');
+    }
+
+    const user = await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return user;
   }
 }
