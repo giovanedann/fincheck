4c82b4fd99163d8a0ec09f3e16394b59ffc2bde7
Giovane <giovanedaniell@gmail.com>
2023-07-02 17:02:45 -0300

    chore: remove unnecessary config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,19 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '../.env',
-    }),
-    UsersModule,
-    DatabaseModule,
-    AuthModule,
-  ],
+  imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [
     {
