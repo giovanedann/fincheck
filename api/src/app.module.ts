a30932c1484696c318f5a1c147bdf86d992dfd71
Giovane <giovanedaniell@gmail.com>
2023-07-02 16:56:31 -0300

    feat: add global AuthGuard provider to protect all routes

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
