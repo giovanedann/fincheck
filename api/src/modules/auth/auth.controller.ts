f538931d6cedfeb04b490ff5ea5decebd15c361b
Giovane <giovanedaniell@gmail.com>
2023-07-03 22:53:37 -0300

    feat: set auth routes as public

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,18 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto, SignUpDto } from './dto';
 
 @Controller('auth')
+@SetMetadata('IS_PUBLIC', true)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  authenticate(@Body() authenticateDto: SignInDto) {
+  signIn(@Body() authenticateDto: SignInDto) {
     return this.authService.signIn(authenticateDto);
   }
 
   @Post('signup')
-  create(@Body() signUpDto: SignUpDto) {
+  signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 }
--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -4,6 +4,7 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
@@ -15,11 +16,17 @@ type JwtPayload = {
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const isPublic = this.reflector.getAllAndOverride<boolean>('IS_PUBLIC', [
+      context.getClass(),
+      context.getHandler(),
+    ]);
 
+    if (isPublic) return true;
+
+    const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
