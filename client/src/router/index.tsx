986ca80d70762abffb03f36c348648f933980a6d
Giovane <giovanedaniell@gmail.com>
2023-08-26 14:01:47 -0300

    [CLIENT] feat: add auth layout

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { AuthGuard } from './AuthGuard'
 import { Dashboard, Login, Register } from '../view/pages'
+import { AuthLayout } from '../view/layouts'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
