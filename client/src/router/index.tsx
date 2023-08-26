d9cea91d966a820503116188ef398d1e86ac1041
Giovane <giovanedaniell@gmail.com>
2023-08-26 13:58:30 -0300

    [CLIENT] feat: add pages base components

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { AuthGuard } from './AuthGuard'
+import { Dashboard, Login, Register } from '../view/pages'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/register" element={<h1>Register</h1>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
-          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="/" element={<Dashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
