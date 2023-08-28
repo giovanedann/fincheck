ca454516c513f1e79b8232037396752f4578a692
Giovane <giovanedaniell@gmail.com>
2023-08-28 13:48:51 -0300

    feat(hooks): add schema validation on useLogin hook

--- client/src/view/pages/Login/hooks/useLogin.ts
+++ client/src/view/pages/Login/hooks/useLogin.ts
@@ -1,15 +1,22 @@
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 type LoginFormData = {
   email: string
   password: string
 }
 
+const schema = z.object({
+  email: z.string().nonempty().email(),
+  password: z.string().nonempty().min(8)
+})
+
 export function useLogin() {
   const { handleSubmit: hookFormHandleSubmit, register } = useForm<LoginFormData>()
 
   const handleSubmit = hookFormHandleSubmit((data) => {
-    console.log({ data })
+    const isDataValid = schema.safeParse(data)
+    console.log({ isDataValid })
   })
 
   return { handleSubmit, register }
