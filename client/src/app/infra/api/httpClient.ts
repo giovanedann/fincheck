94b41740ebabc1944fd45fd2c4119decdb9336f0
Giovane <giovanedaniell@gmail.com>
2023-08-29 18:19:04 -0300

    [CLIENT] feat: add httpClient interceptor to add auth token

--- client/src/app/infra/api/httpClient.ts
+++ client/src/app/infra/api/httpClient.ts
@@ -1,6 +1,18 @@
 import axios from 'axios'
 import { env } from '../../config/env'
+import LocalStorage from '../cache/LocalStorage'
+import { localStorageKeys } from '../../config/localStorageKeys'
 
 export const httpClient = axios.create({
   baseURL: env.apiBaseUrl,
+})
+
+httpClient.interceptors.request.use(config => {
+  const accessToken = LocalStorage.get(localStorageKeys.ACCESS_TOKEN);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 })
