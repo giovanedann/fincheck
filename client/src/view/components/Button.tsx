b530ce37b4d7cb0c7ab9778709fde9bc4b140ea6
Giovane <giovanedaniell@gmail.com>
2023-08-28 23:44:42 -0300

    feat(components): add loading state on Button

--- client/src/view/components/Button.tsx
+++ client/src/view/components/Button.tsx
@@ -1,12 +1,15 @@
 import { ComponentProps } from "react"
 import { cn } from "../../app/utils/cn"
 
-type ButtonProps = ComponentProps<'button'>
+type ButtonProps = ComponentProps<'button'> & {
+  isLoading?: boolean;
+}
 
-export function Button({ className, ...props }: ButtonProps) {
+export function Button({ className, isLoading, disabled, ...props }: ButtonProps) {
   return (
     <button
       {...props}
+      disabled={disabled ?? isLoading}
       className={
         cn(
           'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 transition-all ease-in-out duration-300 disabled:cursor-not-allowed',
