2b098d57dba492f46e9ee20faa13bb23750c446f
Giovane <giovanedaniell@gmail.com>
2023-08-29 20:17:03 -0300

    feat: add response delay simulator on HttpClient

--- client/src/app/infra/api/httpClient.ts
+++ client/src/app/infra/api/httpClient.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { env } from '../../config/env'
 import LocalStorage from '../cache/LocalStorage'
 import { localStorageKeys } from '../../config/localStorageKeys'
+import { sleep } from '../../utils/sleep'
 
 export const httpClient = axios.create({
   baseURL: env.apiBaseUrl,
@@ -15,4 +16,10 @@ httpClient.interceptors.request.use(config => {
   }
 
   return config;
+})
+
+httpClient.interceptors.response.use(async data => {
+  await sleep(1000);
+
+  return data;
 })
