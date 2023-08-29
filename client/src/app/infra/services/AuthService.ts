8b0e2f0bf7234e3f0b29e5cbf8ce0e5a83412b4a
Giovane <giovanedaniell@gmail.com>
2023-08-28 23:43:47 -0300

    feat(utils): add function to simulate delays

--- client/src/app/infra/services/AuthService.ts
+++ client/src/app/infra/services/AuthService.ts
@@ -1,10 +1,12 @@
 import { IAuthService, SignUpParams, SignUpResponse } from "../../domain/services/AuthService";
+import { sleep } from "../../utils/sleep";
 import { httpClient } from "../api/httpClient"
 
 class AuthService implements IAuthService {
   private readonly client = httpClient;
 
   async signUp(params: SignUpParams) {
+    await sleep(1000)
     const { data } = await this.client.post<SignUpResponse>('/auth/signup', params)
 
     return data;
