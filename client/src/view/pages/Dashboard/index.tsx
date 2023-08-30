772cde6a74c7696468998edecfd03260d9a6cc77
Giovane <giovanedaniell@gmail.com>
2023-08-30 13:35:26 -0300

    feat: add some responsiveness to dashboard

--- client/src/view/pages/Dashboard/index.tsx
+++ client/src/view/pages/Dashboard/index.tsx
@@ -3,18 +3,18 @@ import { Accounts, Transactions } from "./components";
 
 export function Dashboard() {
   return (
-    <div className="w-full h-full px-8 pb-8 pt-6 flex flex-col gap-4">
+    <div className="w-full h-full p-4 lg:px-8 lg:pb-8 lg:pt-6 flex flex-col gap-4">
       <header className="h-12 flex items-center justify-between">
         <Logo className="h-6 text-teal-900" />
         <UserMenu name="Benimaru" />
       </header>
 
-      <main className="flex-1 flex gap-4">
-        <div className="w-1/2 h-full">
+      <main className="flex-1 flex flex-col lg:flex-row gap-4">
+        <div className="w-full lg:w-1/2 h-full">
           <Accounts />
         </div>
 
-        <div className="w-1/2 h-full">
+        <div className="w-full lg:w-1/2 h-full">
           <Transactions />
         </div>
       </main>
