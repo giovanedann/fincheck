21fa51aaa03861ad7bad3d6ed1dc7512dc046216
Giovane <giovanedaniell@gmail.com>
2023-08-30 13:35:54 -0300

    feat: add Accounts container and balance styles

--- client/src/view/pages/Dashboard/components/Accounts.tsx
+++ client/src/view/pages/Dashboard/components/Accounts.tsx
@@ -1,5 +1,18 @@
+import { EyeIcon } from "../../../icons";
+
 export function Accounts() {
   return (
-    <div>Accounts</div>
+    <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 lg:p-10">
+      <div>
+        <span className="tracking-[-0.5px] text-white block">Total balance</span>
+
+        <div className="flex items-center gap-2">
+          <strong className="text-2xl tracking-[-1px] text-white">R$ 10.340,00</strong>
+          <button className="w-8 h-8 flex items-center justify-center">
+            <EyeIcon open />
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
