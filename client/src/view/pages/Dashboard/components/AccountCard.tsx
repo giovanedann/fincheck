6bb85b6fc872413ee231e8c72e423a50548eebf1
Giovane <giovanedaniell@gmail.com>
2023-08-31 12:50:14 -0300

    feat: add AccountTypeIcon to AccountCard

--- client/src/view/pages/Dashboard/components/AccountCard.tsx
+++ client/src/view/pages/Dashboard/components/AccountCard.tsx
@@ -1,20 +1,21 @@
 import { formatCurrency } from "../../../../app/utils/formatCurrency";
-import { CategoryIcon } from "../../../icons";
+import { BankAccountTypeIcon } from "../../../icons";
 
 type AccountCardProps = {
   color: string;
   name: string;
   balance: number;
+  type: 'CASH' | 'CHECKING' | 'INVESTMENT'
 }
 
-export function AccountCard({ balance, color, name }: AccountCardProps) {
+export function AccountCard({ balance, color, name, type }: AccountCardProps) {
   return (
     <div
       className="p-4 bg-white rounded-2xl h-[12.5rem] flex flex-col justify-between border-b-4 border-teal-300"
       style={{ borderColor: color }}
     >
       <div>
-        <CategoryIcon type="income" />
+        <BankAccountTypeIcon type={type} />
 
         <span className="text-gray-800 font-medium tracking-[-0.5px] mt-4 block">
           {name}
