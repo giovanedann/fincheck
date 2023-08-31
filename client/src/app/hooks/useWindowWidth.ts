7fd5fe96c05d27d4c79aa1dfa6286da57331b227
Giovane <giovanedaniell@gmail.com>
2023-08-31 18:20:06 -0300

    feat(hooks): add hook to watch window width value

---
+++ client/src/app/hooks/useWindowWidth.ts
@@ -0,0 +1,19 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const handleResize = useCallback(() => {
+    setWidth(window.innerWidth)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [handleResize])
+
+  return width;
+}
