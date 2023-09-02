0ff595cc85978db52a85ce0a3290b58dc03f0348
Giovane <giovanedaniell@gmail.com>
2023-09-02 11:23:04 -0300

    feat: persist values visibility on local storage

--- client/src/view/pages/Dashboard/context/index.tsx
+++ client/src/view/pages/Dashboard/context/index.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, createContext, useCallback, useMemo, useState } from 'react';
+import { localStorageKeys } from 'app/config/localStorageKeys';
+import LocalStorage from 'app/infra/cache/LocalStorage';
+import { ReactNode, createContext, useCallback, useEffect, useMemo, useState } from 'react';
 
 type DashboardContextValues = {
   areValuesVisible: boolean;
@@ -10,8 +12,17 @@ export const DashboardContext = createContext({} as DashboardContextValues)
 export function DashboardProvider({ children }: { children: ReactNode }) {
   const [areValuesVisible, setAreValuesVisible] = useState<boolean>(false)
 
+  useEffect(() => {
+    const localStorageValuesVisibility = LocalStorage.get<boolean>(localStorageKeys.VALUES_VISIBILITY)
+    setAreValuesVisible(localStorageValuesVisibility ?? false)
+  }, [])
+
   const toggleValuesVisibility = useCallback(() => {
-    setAreValuesVisible(prev => !prev)
+    setAreValuesVisible(prev => {
+      alert(!prev)
+      LocalStorage.set(localStorageKeys.VALUES_VISIBILITY, !prev)
+      return !prev
+    })
   }, [])
 
   const contextValues = useMemo(() => ({
