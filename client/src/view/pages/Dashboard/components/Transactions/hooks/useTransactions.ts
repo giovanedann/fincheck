c6089ea857de2cebff6d0809b170600ce9ed0f6a
Giovane <giovanedaniell@gmail.com>
2023-09-03 17:28:37 -0300

    feat(hooks): add modal handlers on useTransactions hook

--- client/src/view/pages/Dashboard/components/Transactions/hooks/useTransactions.ts
+++ client/src/view/pages/Dashboard/components/Transactions/hooks/useTransactions.ts
@@ -1,12 +1,26 @@
+import { useCallback, useState } from 'react';
 import { useDashboard } from 'view/pages/Dashboard/hooks/useDashboard';
 
 export function useTransactions() {
   const { areValuesVisible } = useDashboard()
 
+  const [isFiltersModalOpen, setIsFiltersModalOpen] = useState<boolean>(false)
+
+  const handleOpenFiltersModal = useCallback(() => {
+    setIsFiltersModalOpen(true)
+  }, [])
+
+  const handleCloseFiltersModal = useCallback(() => {
+    setIsFiltersModalOpen(false)
+  }, [])
+
   return {
     areValuesVisible,
     isInitialLoading: false,
     isLoading: false,
-    transactions: [1]
+    transactions: [1],
+    isFiltersModalOpen,
+    handleOpenFiltersModal,
+    handleCloseFiltersModal
   }
 }
