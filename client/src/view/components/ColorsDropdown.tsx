ccfa3e56cabcb145d2dc6f46e78f82983c902cf8
Giovane <giovanedaniell@gmail.com>
2023-09-04 20:02:10 -0300

    feat: add ColorsDropdown component

---
+++ client/src/view/components/ColorsDropdown.tsx
@@ -0,0 +1,93 @@
+import { ChevronDownIcon, CrossCircledIcon } from '@radix-ui/react-icons';
+
+import { Dropdown } from '.';
+
+import { cn } from 'app/utils/cn';
+import { ColorIcon } from 'view/icons';
+import { useState } from 'react';
+
+type Color = {
+  color: string;
+  bg: string;
+};
+
+const colors: Color[] = [
+  { color: '#868E96', bg: '#F8F9FA' },
+  { color: '#FA5252', bg: '#FFF5F5' },
+  { color: '#E64980', bg: '#FFF0F6' },
+  { color: '#BE4BDB', bg: '#F8F0FC' },
+  { color: '#7950F2', bg: '#F3F0FF' },
+  { color: '#4C6EF5', bg: '#EDF2FF' },
+  { color: '#228BE6', bg: '#E7F5FF' },
+  { color: '#15AABF', bg: '#E3FAFC' },
+  { color: '#12B886', bg: '#E6FCF5' },
+  { color: '#40C057', bg: '#EBFBEE' },
+  { color: '#82C91E', bg: '#F4FCE3' },
+  { color: '#FAB005', bg: '#FFF9DB' },
+  { color: '#FD7E14', bg: '#FFF4E6' },
+  { color: '#212529', bg: '#F8F9FA' },
+];
+
+type ColorsDropdownProps = {
+  error?: string;
+  className?: string;
+}
+
+export function ColorsDropdown({ className, error }: ColorsDropdownProps) {
+  const [selectedColor, setSelectedColor] = useState<Color | null>(null)
+
+  function handleSelect(color: Color) {
+    setSelectedColor(color)
+  }
+
+  return (
+    <div>
+      <Dropdown.Root>
+        <Dropdown.Trigger>
+          <button
+            className={
+              cn(
+                'bg-white w-full rounded-lg border border-gray-500 focus:border-gray-800 px-3 h-[52px] text-gray-700 transition-all ease-in-out duration-300 outline-none text-left relative',
+                error && '!border-red-900',
+                className
+              )
+            }
+          >
+            Color
+
+            <div className="absolute right-3 top-1/2 -translate-y-1/2">
+              {!selectedColor && (
+                <ChevronDownIcon className="h-6 w-6 text-gray-800" />
+              )}
+
+              {selectedColor && (
+                <ColorIcon color={selectedColor.color} bg={selectedColor.bg} />
+              )}
+            </div>
+          </button>
+        </Dropdown.Trigger>
+
+        <Dropdown.Content className="grid grid-cols-4">
+          {colors.map((color) => (
+            <Dropdown.Item
+              key={color.color}
+              className="px-4"
+              onSelect={() => handleSelect(color)}
+            >
+              <ColorIcon bg={color.bg} color={color.color} />
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Content>
+      </Dropdown.Root>
+
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-sm">
+            {error}
+          </span>
+        </div>
+      )}
+    </div>
+  )
+}
