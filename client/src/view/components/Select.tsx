4b31be08c28706dc016be835de00576b7cd307c2
Giovane <giovanedaniell@gmail.com>
2023-09-04 17:33:41 -0300

    feat(components): add select component

---
+++ client/src/view/components/Select.tsx
@@ -0,0 +1,96 @@
+import { useCallback, useState } from 'react';
+import * as RdxSelect from '@radix-ui/react-select';
+import { ChevronDownIcon, ChevronUpIcon, CrossCircledIcon } from '@radix-ui/react-icons';
+
+import { cn } from 'app/utils/cn';
+
+type SelectOption = {
+  value: string;
+  label: string;
+}
+
+type SelectProps = {
+  className?: string;
+  error?: string;
+  placeholder?: string;
+  options: SelectOption[]
+}
+
+export function Select({ className, error, placeholder, options }: SelectProps) {
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleSelect = useCallback((value: string) => {
+    setSelectedValue(value)
+  }, [])
+
+  return (
+    <div>
+      <div className="relative">
+        <label
+          className={
+            cn(
+              'absolute z-10 top-1/2 -translate-y-1/2 left-3 text-gray-700 pointer-events-none',
+              selectedValue && 'text-xs left-[13px] top-1.5 transition-all translate-y-0'
+            )
+          }
+        >
+          {placeholder}
+        </label>
+
+        <RdxSelect.Root onValueChange={handleSelect}>
+          <RdxSelect.Trigger
+            className={
+              cn(
+                'bg-white w-full rounded-lg border border-gray-500 focus:border-gray-800 px-3 h-[52px] text-gray-800 transition-all ease-in-out duration-300 outline-none text-left relative pt-4',
+                error && '!border-red-900',
+                className
+              )
+            }
+          >
+            <RdxSelect.Value />
+            <RdxSelect.Icon className="absolute right-3 top-1/2 -translate-y-1/2">
+              <ChevronDownIcon className="w-6 h-6 text-gray-600" />
+            </RdxSelect.Icon>
+          </RdxSelect.Trigger>
+
+          <RdxSelect.Portal>
+            <RdxSelect.Content className="z-[60] overflow-hidden bg-white rounded-2xl border border-gray-100 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.1)]">
+              <RdxSelect.ScrollUpButton
+                className="flex items-center justify-center h-[25px] bg-white text-gray-800"
+              >
+                <ChevronUpIcon />
+              </RdxSelect.ScrollUpButton>
+
+              <RdxSelect.Viewport className="p-2">
+                {options.map((option) => (
+                  <RdxSelect.Item
+                    key={option.value}
+                    className="p-2 text-gray-800 text-sm data-[state=checked]:font-bold outline-none data-[highlighted]:bg-gray-50 rounded-lg transition-all ease-in-out duration-300 cursor-pointer"
+                    value={option.value}
+                  >
+                    <RdxSelect.ItemText>{option.label}</RdxSelect.ItemText>
+                  </RdxSelect.Item>
+                ))}
+              </RdxSelect.Viewport>
+
+              <RdxSelect.ScrollDownButton
+                className="flex items-center justify-center h-[25px] bg-white text-gray-800"
+              >
+                <ChevronDownIcon />
+              </RdxSelect.ScrollDownButton>
+            </RdxSelect.Content>
+          </RdxSelect.Portal>
+        </RdxSelect.Root>
+      </div>
+
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-sm">
+            {error}
+          </span>
+        </div>
+      )}
+    </div>
+  )
+}
