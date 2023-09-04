7db28b1f424fcd3511b7fa4cd20bfdb2b39fa3ac
Giovane <giovanedaniell@gmail.com>
2023-09-04 17:19:16 -0300

    style: add descriptive import to Modal and add styles to CurrencyInput

--- client/src/view/components/CurrencyInput.tsx
+++ client/src/view/components/CurrencyInput.tsx
@@ -3,9 +3,10 @@ import { NumericFormat } from 'react-number-format';
 export function CurrencyInput() {
   return (
     <NumericFormat
-      className="text-gray-900 text-[2rem] font-bold tracking-[-1px] outline-none"
+      className="w-full text-gray-900 text-[2rem] font-bold tracking-[-1px] outline-none"
       thousandSeparator="."
       decimalSeparator=","
+      defaultValue="10,40"
     />
   )
 }
