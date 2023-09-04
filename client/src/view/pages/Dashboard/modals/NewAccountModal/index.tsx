46f37d52c5a30bb44cacdf594e25f375b221c187
Giovane <giovanedaniell@gmail.com>
2023-09-04 17:50:38 -0300

    feat(components): add name and account type input on NewAccountForm

--- client/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ client/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,4 +1,4 @@
-import { Modal } from 'view/components';
+import { CurrencyInput, Input, Modal, Select } from 'view/components';
 import { useNewAccountModal } from './hooks/useNewAccountModal';
 
 export function NewAccountModal() {
@@ -6,7 +6,28 @@ export function NewAccountModal() {
 
   return (
     <Modal title="New account" open={isNewAccountModalOpen} onClose={closeNewAccountModal}>
-      NewAccountModal
+      <form>
+        <div>
+          <span className="text-gray-600 text-xs tracking-[-0.5px]">Balance</span>
+
+          <div className="flex items-center gap-2">
+            <span className="text-gray-600 text-lg tracking-[-0.5px]">$</span>
+            <CurrencyInput />
+          </div>
+        </div>
+
+        <div className="mt-10 flex flex-col gap-4">
+          <Input type="text" name="name" placeholder="Name" />
+          <Select
+            placeholder="Type"
+            options={[
+              { value: 'INVESTMENT', label: 'Investment' },
+              { value: 'CASH', label: 'Cash' },
+              { value: 'CHECKING', label: 'Checking' },
+            ]}
+          />
+        </div>
+      </form>
     </Modal>
   )
 }
