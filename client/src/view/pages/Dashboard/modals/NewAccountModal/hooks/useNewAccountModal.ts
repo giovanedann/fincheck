afaf18aef73cc62c0a94bb2ce56a3eb1f5982023
Giovane <giovanedaniell@gmail.com>
2023-09-15 01:05:34 -0300

    feat: add schema validation to useNewAccountModal

--- client/src/view/pages/Dashboard/modals/NewAccountModal/hooks/useNewAccountModal.ts
+++ client/src/view/pages/Dashboard/modals/NewAccountModal/hooks/useNewAccountModal.ts
@@ -1,10 +1,39 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { useDashboard } from 'view/pages/Dashboard/hooks/useDashboard';
+import { z } from 'zod';
+
+const schema = z.object({
+  initialBalance: z.string().nonempty('Balance is required'),
+  name: z.string().nonempty('Name of account is required'),
+  type: z.enum(['INVESTMENT', 'CHECKING', 'CASH']),
+  color: z.string().nonempty('Color is required')
+})
+
+export type NewAccountFormData = z.infer<typeof schema>;
 
 export function useNewAccountModal() {
   const { closeNewAccountModal, isNewAccountModalOpen } = useDashboard()
 
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control
+  } = useForm<NewAccountFormData>({
+    resolver: zodResolver(schema)
+  })
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    console.log({ data })
+  })
+
   return {
     closeNewAccountModal,
-    isNewAccountModalOpen
+    isNewAccountModalOpen,
+    register,
+    errors,
+    control,
+    handleSubmit
   }
 }
