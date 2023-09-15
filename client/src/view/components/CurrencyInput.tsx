8f68864c318a2e90b14fc4e2b3a7da2557920071
Giovane <giovanedaniell@gmail.com>
2023-09-15 00:16:14 -0300

    feat(components): add props to CurrencyInput to control form values with hookForm

--- client/src/view/components/CurrencyInput.tsx
+++ client/src/view/components/CurrencyInput.tsx
@@ -1,12 +1,37 @@
+import { CrossCircledIcon } from '@radix-ui/react-icons';
+import { cn } from 'app/utils/cn';
 import { NumericFormat } from 'react-number-format';
 
-export function CurrencyInput() {
+type CurrencyInputProps = {
+  error?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+}
+
+export function CurrencyInput({ error, onChange, value }: CurrencyInputProps) {
   return (
-    <NumericFormat
-      className="w-full text-gray-900 text-[2rem] font-bold tracking-[-1px] outline-none"
-      thousandSeparator="."
-      decimalSeparator=","
-      defaultValue="10,40"
-    />
+    <div>
+      <NumericFormat
+        className={
+          cn(
+            'w-full text-gray-900 text-[2rem] font-bold tracking-[-1px] outline-none',
+            error && 'text-red-900'
+          )
+        }
+        value={value}
+        thousandSeparator="."
+        decimalSeparator=","
+        onChange={event => onChange?.(event.target.value)}
+      />
+
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-sm">
+            {error}
+          </span>
+        </div>
+      )}
+    </div>
   )
 }
