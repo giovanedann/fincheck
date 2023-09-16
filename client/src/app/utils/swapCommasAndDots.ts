ecd9d57d95408a849f475c336d0426d30321665e
Giovane <giovanedaniell@gmail.com>
2023-09-16 14:06:01 -0300

    chore: remove unnecessary function

--- client/src/app/utils/swapCommasAndDots.ts
+++
@@ -1,9 +0,0 @@
-export function swapCommasAndDots(inputString: string): string {
-  // Replace all commas with a temporary marker (e.g., '#')
-  const stringWithDots = inputString.replace(/,/g, '#');
-
-  // Replace all dots with commas and all temporary markers with dots
-  const resultString = stringWithDots.replace(/\./g, ',').replace(/#/g, '.');
-
-  return resultString;
-}
